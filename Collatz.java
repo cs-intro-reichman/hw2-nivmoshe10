3d27cd0e257ea46b3ca7c8ea6e80c94bd3264349
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-10 00:20:16 +0200

    clean final Collatz

--- Collatz.java
+++ Collatz.java
@@ -2,17 +2,15 @@
 public class Collatz {
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
-        System.out.print(N);                 
+        System.out.print(N);
 
-        while (N != 1) {    
+        while (N != 1) {
+            if (N % 2 == 0)
+                N = N / 2;
+            else
+                N = 3 * N + 1;
 
-            if (N % 2 == 0) 
-			N = N / 2;
-
-        	else  
-			N = 3 * N + 1;
-
-            System.out.print(" " + N);      
+            System.out.print(" " + N);
         }
     }
 }
