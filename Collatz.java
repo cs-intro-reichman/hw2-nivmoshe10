1d2ade0b2e554e12c7ca1c7a5b05d994ec8f85c5
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-09 23:34:07 +0200

    fix Collatz output spacing

--- Collatz.java
+++ Collatz.java
@@ -2,15 +2,15 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    int N = Integer.parseInt(args[0]);
-		System.out.print(N + " ");
+		System.out.print(N);
 		for(int i = 0; N != 1; i++) {
 			if (N % 2 == 0) { 
 				N = N / 2;
-				System.out.print(N + " ");
+				System.out.print(" " + N);
 			}
 			else {
 				N = (N * 3) + 1;
-				System.out.print(N + " ");
+				System.out.print(" " + N);
 			}
 		}
 	}
