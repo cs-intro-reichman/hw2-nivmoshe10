bb35aa4ee06f0a4fc39c7c46084fbe554cc816eb
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-09 22:14:31 +0200

    add Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,31 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String name = args[0].toUpperCase();
+        int num = Integer.parseInt(args[1]);
+        int L = name.length();
+        String an = "AEFHILMNORSX";
+
+        for (int i = 0; i < L; i++) {
+        char ch = name.charAt(i); 
+
+        if (an.indexOf(ch) != -1) { 
+        
+                System.out.println("Give me an " + ch + ": " + ch + "!");
+        } 
+        
+        else { 
+       
+                System.out.println("Give me a  " + ch + ": " + ch + "!");
+                }
+        }
+                System.out.println("What does that spell?");
+
+                
+        for (int i = 0; i < num; i++) {
+                System.out.println(name + "!!!");
+
+
+                }
         }
 }
