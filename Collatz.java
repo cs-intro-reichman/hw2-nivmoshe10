1930212f54795fb95e8f7b35d1b9e3c7be3094b0
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-10 00:09:41 +0200

    final: Collatz while + fix CalcPi

--- Collatz.java
+++ Collatz.java
@@ -1,16 +1,18 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-    public static void main(String args[]) {
-    int N = Integer.parseInt(args[0]);
-    System.out.print(N); 
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        System.out.print(N);                 
 
-        while (N != 1) { 
-            if (N % 2 == 0) {
-                N = N / 2;
-            } else {
-                N = (N * 3) + 1;
-            }
-            System.out.print(" " + N); 
+        while (N != 1) {    
+
+            if (N % 2 == 0) 
+			N = N / 2;
+
+        	else  
+			N = 3 * N + 1;
+
+            System.out.print(" " + N);      
         }
     }
 }
