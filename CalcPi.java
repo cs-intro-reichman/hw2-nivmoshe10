418a66e26b008b7c3e7c64a54bb172d63d70c592
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-09 22:49:22 +0200

    add CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	int N = Integer.parseInt(args[0]);
+	double sum = 0.0;
+	for (int i = 0; i < N; i++) {
+		double p = (1.0/((2 * i) + 1));
+		
+		if(i % 2 == 0) { 
+			sum = sum + p;
+		}
+		else {
+			sum = sum - p;
+		} 
 	}
-}
+		double pi = 4 * sum;
+		System.out.println(pi);
+
+		}
+	}
+
