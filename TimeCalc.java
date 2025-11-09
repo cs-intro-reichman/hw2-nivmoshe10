1027693d0220d4f540249b8dd674b5a0d85a2579
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-09 18:04:07 +0200

    HW2: implement TimeCalc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,23 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+	String time = args[0];
+	
+    String[] parts = time.split(":");
+	int hh = Integer.parseInt(parts[0]);
+	int mm= Integer.parseInt(parts[1]);
+
+    int time2 = Integer.parseInt(args[1]);
+
+
+	int allminutes = (((hh * 60) + mm) + time2);
+
+	int clock1 = (allminutes / 60);
+    int clock11 = (clock1 % 24);
+
+    int clock2 = (allminutes % 60);
+
+    System.out.println(clock11 + ":" + clock2);
+
+
     }
 }
