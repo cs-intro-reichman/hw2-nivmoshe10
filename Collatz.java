1db854f87c8acd60422383d1f6718999f453082b
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-09 23:19:30 +0200

    add Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,17 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+		System.out.print(N + " ");
+		for(int i = 0; N != 1; i++) {
+			if (N % 2 == 0) { 
+				N = N / 2;
+				System.out.print(N + " ");
+			}
+			else {
+				N = (N * 3) + 1;
+				System.out.print(N + " ");
+			}
+		}
 	}
 }
