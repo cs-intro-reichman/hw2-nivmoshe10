ec4ebbae38315dfc943c25a21de5557ec85b2032
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-10 10:41:09 +0200

    Add Collatz.java final version

--- Collatz.java
+++ Collatz.java
@@ -6,20 +6,29 @@ public class Collatz {
             int num = i;                 
             int steps = 1;               
 
-            System.out.print(i);  
+            System.out.print(i);
 
-            while (num != 1) {           
-                if (num % 2 == 0) {
-                    num = num / 2;       
-                } else {
-                    num = 3 * num + 1;   
+            if (num == 1) {
+                System.out.print(" 4 2 1");
+                steps = 4;
+            } 
+			else {
+                while (num != 1) {           
+                   if (num % 2 == 0) {
+                        num = num / 2;       
+                    } 
+					else {
+                        num = 3 * num + 1;   
+                    }
+
+                    System.out.print(" " + num);
+                    steps++;                 
                 }
-
-                System.out.print(" " + num);
-                steps++;                 
             }
+
             System.out.println(" (" + steps + ")"); 
         }
+
         System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
     }
- }
+}
