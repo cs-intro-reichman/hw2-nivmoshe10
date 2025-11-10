10c749c5950963d15f4b23dc73506a2d1a8c282c
Niv Moshe <niv.moshe@post.runi.ac.il>
2025-11-10 09:32:54 +0200

    testing

--- CalcPi.java
+++ CalcPi.java
@@ -13,14 +13,7 @@ public class CalcPi {
 			sum -= p;
         }
         	double pi = 4 * sum;
-			// System.out.println("Pi according to Java: " + Math.PI);
-			// System.out.println("Pi, approximated:     " + pi);
 			System.out.println("pi according to Java: "+Math.PI+"\npi, approximated:     "+pi);
     }
 }
 
-::error::The output for test CalcPi test 1: Args (1) did not match%0AExpected:%0A
-pi according to Java: 3.141592653589793
-pi, approximated:     4.0
-pi according to Java: 3.141592653589793
-pi, approximated:     4.0
